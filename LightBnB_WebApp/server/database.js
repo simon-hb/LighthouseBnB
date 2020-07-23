e1d0e64e2b72f61001e4aedb28a7bd8a79330df3
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-23 18:24:37 +0000

    my reservations lists reservations for user

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -86,7 +86,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.*, reservations.*, AVG (property_reviews.rating) AS average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+`, [guest_id, limit])
+.then(res => res.rows)
+.catch(err => null);
+  // return getAllProperties(null, 2); (old code)
 }
 exports.getAllReservations = getAllReservations;
 
